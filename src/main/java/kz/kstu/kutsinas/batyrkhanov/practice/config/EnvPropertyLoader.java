745c3a6f07eb3cc1cc5019890f9113d107b94db2
Armando <159049463+Philinnnn@users.noreply.github.com>
2025-05-07 17:13:15 +0500

    Env add

---
+++ src/main/java/kz/kstu/kutsinas/batyrkhanov/practice/config/EnvPropertyLoader.java
@@ -0,0 +1,20 @@
+package kz.kstu.kutsinas.batyrkhanov.practice.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnvPropertyLoader {
+
+    @PostConstruct
+    public static void init() {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        dotenv.entries().forEach(entry -> {
+            if (System.getProperty(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
+    }
+}
--- src/main/java/kz/kstu/kutsinas/batyrkhanov/practice/runner/PlaylistsManager.java
+++ src/main/java/kz/kstu/kutsinas/batyrkhanov/practice/runner/PlaylistsManager.java
@@ -1,11 +1,13 @@
 package kz.kstu.kutsinas.batyrkhanov.practice.runner;
 
+import kz.kstu.kutsinas.batyrkhanov.practice.config.EnvPropertyLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlaylistsManager {
     public static void main(String[] args){
+        EnvPropertyLoader.init();
         SpringApplication.run(PlaylistsManager.class,args);
     }
 }
