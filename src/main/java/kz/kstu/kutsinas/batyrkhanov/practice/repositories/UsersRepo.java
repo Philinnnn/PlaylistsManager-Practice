61dc0538e6d9cce0943beef51fdb06eb9046f3c1
Armando <159049463+Philinnnn@users.noreply.github.com>
2025-05-08 21:02:13 +0500

    users -> spotify_users rename

--- src/main/java/kz/kstu/kutsinas/batyrkhanov/practice/repositories/UsersRepo.java
+++ src/main/java/kz/kstu/kutsinas/batyrkhanov/practice/repositories/UsersRepo.java
@@ -1,6 +1,6 @@
 package kz.kstu.kutsinas.batyrkhanov.practice.repositories;
 
-import kz.kstu.kutsinas.batyrkhanov.practice.entities.User;
+import kz.kstu.kutsinas.batyrkhanov.practice.entities.SpotifyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UsersRepo extends JpaRepository<User,String> {
+public interface UsersRepo extends JpaRepository<SpotifyUser,String> {
     @NonNull
-    Optional<User> findById(String id);
-    Optional<User> findByEmail(String email);
+    Optional<SpotifyUser> findById(String id);
+    Optional<SpotifyUser> findByEmail(String email);
 }
